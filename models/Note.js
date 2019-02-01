a5e8daf959115415c5b0da9f587bcd0920a375db
That_Guy <eghove@gmail.com>
2019-02-01 16:49:09 -0600

    setting up the Note.js model

--- models/Note.js
+++ models/Note.js
@@ -4,3 +4,19 @@
 // Require mongoose
 var mongoose = require('mongoose');
 
+// Reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// the Note Schema
+var NoteSchema = new Schema({
+  // title field of the document
+  title: String,
+  // body field of the document
+  body: String
+});
+
+// create the model from the above schema
+var Note = mongoose.model('Note', NoteSchema);
+
+// export the Note model
+module.exports = Note;
