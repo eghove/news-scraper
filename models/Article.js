1948d9f63af5630de6a03c49f57dfd21c23947f7
That_Guy <eghove@gmail.com>
2019-02-01 16:44:58 -0600

    setting up Article.js model

---
+++ models/Article.js
@@ -0,0 +1,48 @@
+/* eslint-disable semi */
+// schema for the Article collection in the database
+
+// Require mongoose
+var mongoose = require('mongoose');
+
+// Reference to the schema constructor
+var Schema = mongoose.Schema;
+
+// the Article Schema
+var ArticleSchema = new Schema({
+  // field to store the title of the article
+  title: {
+    type: String,
+    required: true
+  },
+  // field to store the link of the article
+  link: {
+    type: String,
+    required: true
+  },
+  // field to store the summary of the article
+  summary: {
+    type: String,
+    required: true
+  },
+  // field to store byline
+  byline: {
+    type: String,
+    required: true
+  },
+  // field that stores a Note id
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: 'Note'
+  },
+  // field that will store if the article has already been read
+  read: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// create the model from the above schema
+var Article = mongoose('Article', ArticleSchema);
+
+// expore the model
+module.exports = Article;
---
+++ models/Note.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+// schema for the Note collection in the database
+
+// Require mongoose
+var mongoose = require('mongoose');
+
