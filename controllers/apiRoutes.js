855bd92e02f8ce742501ce3821df8dd24c4a4ed1
That_Guy <eghove@gmail.com>
2019-02-02 07:05:19 -0600

    putting in the GET route for all articles in the collection

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -33,7 +33,7 @@ module.exports = function (app) {
         results.byline = byline;
         results.summary = summary;
         results.link = link;
-        // create a new Article using the `results` object build from scraping
+        // create a new Article using the `results` object built from scraping
         db.Article.create(results)
           .then(function (dbArticle) {
             console.log(dbArticle);
@@ -42,14 +42,24 @@ module.exports = function (app) {
             // log the error if needed
             console.log(err);
           });
-        // results.push({
-        //   title: title,
-        //   summary: summary,
-        //   byline: byline,
-        //   link: link
-        // });
       });
     })
+    // let the client know the scraping is done. Probably want to change this to a status code at some point.
     res.send('Scraped!')
+  });
+
+  // GET route for getting all Articles from the database
+  app.get('/articles', function (req, res) {
+    // grab all documents in the Articles collection
+    db.Article.find({})
+      .then(function (dbArticle) {
+        // if successful, send all the article back to the client as a JSON object
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        // if an error occurred, send it to the cliend
+        res.json(err);
+      })
   })
+
 }
