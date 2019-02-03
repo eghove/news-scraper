4c3b65cf97a40a70cebf0cfd9a69c6a6ce1c916a
That_Guy <eghove@gmail.com>
2019-02-03 15:47:39 -0600

    fixing the containers for the article display

--- public/js/logic.js
+++ public/js/logic.js
@@ -10,9 +10,30 @@ $(document).ready(function () {
       type: 'GET'
     }).then(
       function () {
-        // reload the page
+        // reload the page (this doesn't seem to be working)
         location.reload();
       }
     );
+  })
+
+  // event listener when someone clicks on an article
+  $('.article').on('click', function (event) {
+    // empty the note display
+    $('.note-display').empty();
+    let thisID = $(this).attr('data-article-id');
+    // AJAX call for the article
+    $.ajax({
+      method: 'GET',
+      url: '/articles/' + thisID
+    })
+      .then(function (data) {
+        // console.log(data);
+        // build a card
+        let card = $('.note-display').append('<div class = "card-body" >');
+        let inputField = $('<input id = "title-input" name = "title" >');
+        card.append(inputField);
+
+
+      })
   })
 });
