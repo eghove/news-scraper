32aee57d7f164a50d85a098a06da6e65b963da5f
That_Guy <eghove@gmail.com>
2019-02-03 16:39:30 -0600

    getting closer

--- public/js/logic.js
+++ public/js/logic.js
@@ -21,26 +21,32 @@ $(document).ready(function () {
     // empty the note display
     $('.note-display').empty();
     let thisID = $(this).attr('data-article-id');
-    // where we'll put the notecard
+    // find the selector for note-display
     let noteCard = $(this).children('div.row').find('div.note-display');
+    // build the card and card-body
     let card = $('<div class = "card">');
     let cardBody = $('<div class = "card-body">')
-    card = card.append(cardBody);
-    noteCard.append(card);
+    // append card-body to card
+    // card = card.append(cardBody);
+    // append modified card to noteCard
+    // noteCard.append(card);
     
-    // noteCard.append('<h2>TEST</h2>');
     // AJAX call for the article
     $.ajax({
       method: 'GET',
       url: '/articles/' + thisID
     })
       .then(function (data) {
-        // console.log(data);
-
-      
-        
-
-
+        console.log(data);
+        // an input to enter a new title for the note
+        let noteTitle = $('<input id = "title-input" name = "title" >')
+        // an input the enter the body of the note
+        let noteBody = $('<textarea id = "body-input" name = "body">')
+        // a button to the submit the new note, with the id of article save to it
+        let noteButton = $("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        cardBody = cardBody.append(noteTitle).append(noteBody).append(noteButton);
+        card = card.append(cardBody);
+        noteCard.append(card);
       })
   })
 });
